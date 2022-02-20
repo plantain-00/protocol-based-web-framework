166d854ad1313daddcf351f83314b0d653ead669
York Yao <me@yorkyao.com>
2022-02-20 08:08:44 +0800

    fix: publish error

--- clean-release.config.ts
+++ clean-release.config.ts
@@ -2,8 +2,8 @@ import { Configuration } from 'clean-release'
 
 const config: Configuration = {
   include: [
-    'packages/*/dist/*',
-    'packages/*/cjs/*',
+    'packages/*/dist/**/*',
+    'packages/*/cjs/**/*',
     'packages/*/bin/*',
     'packages/*/package.json',
     'packages/*/README.md',
